0e7eb2d51bff99a70ae8664fc573da9297bb1744
merki <merki@DESKTOP-2QS1E2U.lan>
2020-11-12 19:47:52 -0700

    add names

---
+++ CS159_RegionalTwitterClassifier/src/nlp/nbc/ModelTrainer.java
@@ -0,0 +1,19 @@
+package nlp.nbc;
+
+/**
+ * 
+ * @author Magali Ngouabou, Helen Paulini, Mercy Bickell
+ * CS159 - Final Project
+ *
+ */
+public class ModelTrainer {
+	
+	public ModelTrainer() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
