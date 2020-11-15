511015aad59e926350086075c23aaf525ab68201
merki <merki@DESKTOP-2QS1E2U.lan>
2020-11-14 17:35:24 -0700

    import stanford tokenizer to tokenize tweet text

--- CS159_RegionalTwitterClassifier/src/nlp/nbc/ModelTrainer.java
+++ CS159_RegionalTwitterClassifier/src/nlp/nbc/ModelTrainer.java
@@ -1,5 +1,10 @@
 package nlp.nbc;
 
+import java.io.*;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+
 /**
  * 
  * @author Magali Ngouabou, Helen Paulini, Mercy Bickell
@@ -7,13 +12,36 @@ package nlp.nbc;
  *
  */
 public class ModelTrainer {
-	
-	public ModelTrainer() {
-		
+
+	public ModelTrainer(String filename) {
+		try {
+			BufferedReader trainingDataReader = new BufferedReader(new FileReader(filename));	
+			String tweetLine = trainingDataReader.readLine();
+
+			while (tweetLine != null) {
+
+				String[] splitLine = tweetLine.split("\t");
+				StringReader tweetText = new StringReader(splitLine[2]);
+
+				PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(tweetText,
+						new CoreLabelTokenFactory(), "americanize=false");
+				while (ptbt.hasNext()) {
+					CoreLabel label = ptbt.next();
+					System.out.println(label);
+					// Add to appropriate hashtable entry
+				}
+				tweetLine = trainingDataReader.readLine();
+			}
+
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 	
 	public static void main(String[] args) {
-		
+		ModelTrainer tester = new ModelTrainer("data/testFile.txt");
 	}
 	
 }
