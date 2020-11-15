4602f623260505b7c8e75123a39efe94edcd207a
merki <merki@DESKTOP-2QS1E2U.lan>
2020-11-14 17:54:35 -0700

    added data structures

--- CS159_RegionalTwitterClassifier/src/nlp/nbc/ModelTrainer.java
+++ CS159_RegionalTwitterClassifier/src/nlp/nbc/ModelTrainer.java
@@ -1,6 +1,9 @@
 package nlp.nbc;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
@@ -12,7 +15,17 @@ import edu.stanford.nlp.process.CoreLabelTokenFactory;
  *
  */
 public class ModelTrainer {
-
+	
+	// Maps from string of twitter user ID to location of user
+	HashMap<String, String> idLocations = new HashMap<>();
+	
+	HashMap<String, HashMap<String, Integer>> labelWords = new HashMap<>();
+	HashMap<String, Double> labelProbs = new HashMap<>();
+	HashSet<String> vocab = new HashSet<>();
+	HashMap<String, Integer> labelWordCounts = new HashMap<>();
+	double lambda = 0;
+	
+	
 	public ModelTrainer(String filename) {
 		try {
 			BufferedReader trainingDataReader = new BufferedReader(new FileReader(filename));	
@@ -22,8 +35,11 @@ public class ModelTrainer {
 
 				String[] splitLine = tweetLine.split("\t");
 				
+				// Stop list processing???
+				// Remove hashtags and treat the hashtag as a normal word?
+				
 				// Pass in lower-cased tweet text to String Reader so that in
-				// can be tokenized
+				// can be tokenized 
 				StringReader tweetText = new StringReader(splitLine[2].toLowerCase());
 
 				PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(tweetText,
